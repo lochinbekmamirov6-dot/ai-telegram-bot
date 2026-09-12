627ea954ee8844baa66052951ee50e13f0c18800
lochinbekmamirov6-dot <lochinbekmamirov6@gmail.com>
2026-09-12 15:54:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def handle_ai_message(message: types.Message):
 
   try:
     response = await client.chat.completions.create(
-        model="llama-3.3-70b-8192",
+        model="llama-3.1-8b-instant",
         messages=[
             {
                 "role": "system",
